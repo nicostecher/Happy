e1a1b445787e5cd784e932c5802355424ef9ad3c
nico stecher <nicostecher1@gmail.com>
2019-11-11 16:59:37 -0300

    otroo comit trucho

--- clases/autoload.php
+++ clases/autoload.php
@@ -1,11 +1,6 @@
-<?
-<<<<<<< HEAD
+<?php
 session_start();
-require_once("validador.php");
-=======
-session_start()
 require_once("ValidadorLogin.php");
->>>>>>> 0dab5bf030dc8eb88a87ea8c1530f4c2e16242e5
 require_once("productos.php");
 require_once("usuario.php");
 require_once("tarjetas.php");
@@ -14,3 +9,4 @@ require_once("carritoClase.php");
 require_once("registroClase.php");
 require_once("loginClase.php");
 require_once("baseDatos.php");
+?>
--- clases/validarregistro.php
+++ clases/validarregistro.php
@@ -8,6 +8,7 @@ private $password;
 private $avatar;
 private $nombre;
 private $apellido;
+private $errores=[];
 
 //CONSTRUCT//
 public function __construct(string $email,string $password, string $avatar,string $nombre, string $apellido){
@@ -16,6 +17,29 @@ public function __construct(string $email,string $password, string $avatar,strin
   $this->avatar=$avatar;
   $this->nombre=$nombre;
   $this->apellido=$apellido;
+}
+  public function validarNombre(){
+      if(strlen($this->nombre)==0){
+        $errores["nombre"][]="completa el nombre";
+      }
+    }
+      public function validarApellido(){
+          if(strlen($this->apellido)==0){
+            $errores["apellido"][]="completa el apellido";
+          }
+      }
+
+      public function validarEmail(){
+          if(strlen($this->email)==0){
+              $errores["email"][]="no completaste el mail";
+          }
+        if(!filter_var($this->email, FILTER_VALIDATE_MAIL)){
+          $errores["email"][]="el formato de email no es el correcto";
+
+          }
+      }
+
+      public function
 }
 
 
